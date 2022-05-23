c80112b66ab3332116c8a1eca926db2e7884f13e
midpushgogo <790210592@qq.com>
2022-05-23 17:35:19 +0800

    d

--- train.sh
+++
@@ -1,9 +0,0 @@
-model=acvsgnet
-datapath=data
-batch_size=4
-logdir=log/gwcsg_self_lr0.001_log
-loadckpt=none
-lr=0.001
-test_batch_size=2	
-python3 main.py --model $model --datapath $datapath --batch_size $batch_size \
---logdir $logdir --loadckpt $loadckpt --test_batch_size $test_batch_size --lr $lr --self_supervised 
--- valid.sh
+++
@@ -1,8 +0,0 @@
-model=acvsgnet
-datapath=data
-batch_size=6
-logdir=valid_log
-loadckpt=log/acvsg_conbine_refine_log/checkpoint_000_epe_2.049_42.047.ckpt
-test_batch_size=1	
-python3 valid.py --model $model --datapath $datapath --batch_size $batch_size \
---logdir $logdir --loadckpt $loadckpt --test_batch_size $test_batch_size
